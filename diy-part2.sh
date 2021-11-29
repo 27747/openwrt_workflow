556ede41666362f414344abf97242ab89919ba2c
27747 <318394576@qq.com>
2021-11-29 21:37:28 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,15 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.50.2/g' package/base-files/files/bin/config_generate删除原主题
+
+rm -rf package/lean/luci-theme-argon
+##添加新的主题
+
+git clone https://github.com/sypopo/luci-theme-atmaterial.git package/lean/luci-theme-atmaterial
+取消bootstrap为默认主题
+
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+#删除默认密码
+
+sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
